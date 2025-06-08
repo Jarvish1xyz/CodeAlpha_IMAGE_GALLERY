55880388b6e8fc3e8775ef7924802353aa521d8e
Sinha Milan <milans0415@gmail.com>
2025-06-08 20:02:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 function bigShow(name) {
     name+='1';
-    document.getElementById(name).style.display = "flex";
-    document.getElementById(name).style.animationName = "first";
+    const myImage1 = document.getElementById(name);
+    myImage1.style.display = "flex";
+    myImage1.style.animationName = "first";
+    document.getElementById("")
+    history.pushState({image : true}, "");
 
     document.addEventListener('keydown', function(e) {
         if(e.key==='Escape') {
-            document.getElementById(name).style.display = "none";
-            document.getElementById(name).style.animationName = "";
+            myImage1.style.display = "none";
+            myImage1.style.animationName="";
         }
-    })
+    });
+
+    myImage1.addEventListener('click', (e) => {
+        if (e.target === myImage1) {
+          myImage1.style.display = "none";
+          myImage1.style.animationName="";
+        }
+    });
+
+    window.addEventListener('popstate', (event) => {
+        if (myImage1.style.display === 'flex') {
+            myImage1.style.display = "none";
+            myImage1.style.animationName="";
+        }
+    });
+}
+
+function closePhoto() {
 }
